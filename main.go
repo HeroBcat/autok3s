fe03b544588133a6b405e723c25f227ff465f64a
herobcat <HeroBcat@users.noreply.github.com>
2020-11-05 15:07:59 +0800

    feat: add more flags

--- main.go
+++ main.go
@@ -12,12 +12,17 @@ import (
 func main() {
 
 	var (
-		username     string
-		password     string
-		masterIPs    []string
-		workerIPs    []string
-		authKeysPath string
-		withDocker   bool
+		username          string
+		password          string
+		masterIPs         []string
+		workerIPs         []string
+		authKeysPath      string
+		preMasterCommands []string
+		preWorkerCommands []string
+		masterCommands    []string
+		workerCommands    []string
+		masterExtraArgs   string
+		workerExtraArgs   string
 	)
 
 	rootCmd := &cobra.Command{
@@ -52,11 +57,17 @@ func main() {
 					}
 				}
 
+				for _, cmd := range preMasterCommands {
+					runCommands(ip, username, password, cmd)
+				}
+
 				runCommands(ip, username, password, "sudo apt install nfs-common -y")
 				k3sCmd := "curl -sfL http://rancher-mirror.cnrancher.com/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn sh -s - --tls-san " + ip
-				if withDocker {
+				if strings.Contains(masterExtraArgs, "--docker") {
 					runCommands(ip, username, password, "sudo apt install docker.io -y")
-					k3sCmd += " --docker"
+				}
+				if workerExtraArgs != "" {
+					k3sCmd += " " + workerExtraArgs
 				}
 				runCommands(ip, username, password, k3sCmd)
 
@@ -65,6 +76,10 @@ func main() {
 				k3sConfig := getCommandsOutput(ip, username, password, "sudo cat -s /etc/rancher/k3s/k3s.yaml")
 				k3sConfig = strings.Replace(k3sConfig, "127.0.0.1", ip, 1)
 				ioutil.WriteFile("k3s.yaml", []byte(k3sConfig), os.ModePerm)
+
+				for _, cmd := range masterCommands {
+					runCommands(ip, username, password, cmd)
+				}
 			}
 
 			for _, ip := range workerIPs {
@@ -74,13 +89,23 @@ func main() {
 					runCommands(ip, username, password, "echo "+split+" >> /home/"+username+"/.ssh/authorized_keys")
 				}
 
+				for _, cmd := range preWorkerCommands {
+					runCommands(ip, username, password, cmd)
+				}
+
 				runCommands(ip, username, password, "sudo apt install nfs-common -y")
 				k3sCmd := "curl -sfL http://rancher-mirror.cnrancher.com/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn sh -s - agent --server " + k3sURL + " --token " + k3sToken
-				if withDocker {
+				if strings.Contains(workerExtraArgs, "--docker") {
 					runCommands(ip, username, password, "sudo apt install docker.io -y")
-					k3sCmd += " --docker"
+				}
+				if workerExtraArgs != "" {
+					k3sCmd += " " + workerExtraArgs
 				}
 				runCommands(ip, username, password, k3sCmd)
+
+				for _, cmd := range masterCommands {
+					runCommands(ip, username, password, cmd)
+				}
 			}
 
 		},
@@ -90,8 +115,13 @@ func main() {
 	rootCmd.Flags().StringVarP(&password, "password", "p", "", "Specify the password of the server")
 	rootCmd.Flags().StringSliceVarP(&masterIPs, "master_ips", "m", nil, "Specify the ip of the master servers")
 	rootCmd.Flags().StringSliceVarP(&workerIPs, "worker_ips", "w", nil, "Specify the ip of the worker servers")
-	rootCmd.Flags().StringVarP(&authKeysPath, "id_rsa_path", "i", "", "Specify the path of id rsa content")
-	rootCmd.Flags().BoolVar(&withDocker, "docker", false, "Specify whether to use docker")
+	rootCmd.Flags().StringVarP(&authKeysPath, "auth_keys_path", "k", "", "Specify the path of id rsa content")
+	rootCmd.Flags().StringSliceVar(&preMasterCommands, "pre_master_commands", nil, "Specify the commands that need to be pre-installed on the master server")
+	rootCmd.Flags().StringSliceVar(&preWorkerCommands, "pre_worker_commands", nil, "Specify the commands that need to be pre-installed on the worker server")
+	rootCmd.Flags().StringSliceVar(&masterCommands, "master_commands", nil, "Specify the commands that need to be installed on the master server")
+	rootCmd.Flags().StringSliceVar(&workerCommands, "worker_commands", nil, "Specify the commands that need to be installed on the worker server")
+	rootCmd.Flags().StringVar(&masterExtraArgs, "master_extra_args", "", "Specify the extra args on the master server")
+	rootCmd.Flags().StringVar(&workerExtraArgs, "worker_extra_args", "", "Specify the extra args on the worker server")
 
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
